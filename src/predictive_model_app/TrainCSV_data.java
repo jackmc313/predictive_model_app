76bc0451022d67eb8b35c3c0c3974ce9988ab04e
jackmc313 <C23323661@mytudublin.ie>
2025-04-28 18:54:55 +0100

    training the csv data

--- src/predictive_model_app/TrainCSV_data.java
+++ src/predictive_model_app/TrainCSV_data.java
@@ -1,5 +1,35 @@
 package predictive_model_app;
 
+import java.util.*;
+
 public class TrainCSV_data {
+	
+	private Map<String, Integer> lineCount = new HashMap<>();
+	private String lineOfData;
+	
+	
+	//method to divide data up to yes and nos 
+	public void train(List<String> csvData)
+	{
+		
+		lineCount.clear();
+		
+		for(String lineOfData : csvData)
+		{
+			if (lineCount.containsKey(lineOfData))
+			{
+				lineCount.put(lineOfData, lineCount.get(lineOfData) + 1);
+			}
+			else 
+			{
+				lineCount.put(lineOfData, 1);
+			}
+		}
+	}
+	
+	
+	
+	
+	
 
 }
