974c80c612edc5831901c2eae0f66212490afd01
jackmc313 <C23323661@mytudublin.ie>
2025-04-28 22:05:58 +0100

    TrainCSV nearly finished

--- src/predictive_model_app/TrainCSV_data.java
+++ src/predictive_model_app/TrainCSV_data.java
@@ -4,10 +4,25 @@ import java.util.*;
 
 public class TrainCSV_data {
 	
-	private Map<String, Integer> lineCount = new HashMap<>();
+	private Map<String, Integer> lineCount;
 	private String lineOfData;
+	private boolean equal;
+	private String splitUserInput[];
+	private String splitCsvData[];
+	private String singleData;
+	private int yes;
+	private int no;
+	private String result;
+	private int percentage;
 	
 	
+	
+	
+	public TrainCSV_data ()
+	{
+		lineCount = new HashMap<>();
+		yes =
+	}
 	//method to divide data up to yes and nos 
 	public void train(List<String> csvData)
 	{
@@ -25,6 +40,67 @@ public class TrainCSV_data {
 				lineCount.put(lineOfData, 1);
 			}
 		}
+	}
+	
+	public String forecastResult(String enteredValues)
+	{
+		yes = 0;
+		no = 0;
+		splitUserInput = enteredValues.split(",");
+		
+		//splitting up Csv data
+		for (String singleData : lineCount.keySet())
+		{
+			equal = true;
+			splitCsvData = singleData.split(",");
+			
+			//checking split if split data is equal
+			for (int i = 0; i < splitUserInput.length; i++)
+			{
+				if (!splitCsvData[i].equals(splitUserInput[i]))
+				{
+					equal = false;
+					break;
+				}
+			}
+			
+			if (equal) 
+			{
+				if (splitCsvData[splitCsvData.length - 1].equals("yes"))
+				{
+					yes += lineCount.get(singleData);
+				}
+				else if (splitCsvData[splitCsvData.length - 1].equals("no"))
+				{
+					no += lineCount.get(singleData);
+				}
+			}
+		}
+		
+		//checking whether user is verified or not
+		if (yes > no)
+		{
+			result = "User is verified";
+		}
+		else if (no > yes)
+		{
+			result = "User is not verified";
+		}
+		else 
+		{
+			result = "Data shows an equal chance of being verified or not";
+		}
+		
+		//calculating percentage using math.round to round to nearest whole number
+		percentage = (int) Math.round(((double) Math.max(yes, no) / (yes + no)) * 100);
+		
+		
+		
+		
+		
+		
+		
+		return result + " Percentage = " + percentage + "%";
 	}
 	
 	
