3caec4db54dbaf3252777bc87e0faa22ab8c2669
jackmc313 <C23323661@mytudublin.ie>
2025-04-28 17:14:22 +0100

    saving

--- src/predictive_model_app/FileManagementLevel2.java
+++ src/predictive_model_app/FileManagementLevel2.java
@@ -1,5 +1,40 @@
 package predictive_model_app;
 
-public class FileManagementLevel2 {
+import java.io.*;
+import java.util.*;
 
+public class FileManagementLevel2 {
+	
+	private static List<String> fileData;
+	private static Scanner myScanner;
+	private static File fileName;
+	private static String cutUpData;
+	
+	
+	
+	public static List<String> takeInData(String filePath)
+	{
+		fileData = new ArrayList<>();
+		
+		try {
+			fileName = new File(filePath);
+			myScanner = new Scanner(fileName);
+			
+			while (myScanner.hasNextLine())
+			{
+				cutUpData = myScanner.nextLine();
+				fileData.add(cutUpData);
+				
+			}
+			
+		} catch(FileNotFoundException e)
+		{
+			System.out.println("Error, file not found");
+		}
+		
+		
+		
+		return fileData;
+	}
+	
 }
