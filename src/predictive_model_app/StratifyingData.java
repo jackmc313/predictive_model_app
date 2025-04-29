6a1c21f40d7a9f322a3641ac2f86711fa3b42ba5
jackmc313 <C23323661@mytudublin.ie>
2025-04-29 15:09:09 +0100

    Stratifying data nearly complete

---
+++ src/predictive_model_app/StratifyingData.java
@@ -0,0 +1,62 @@
+package predictive_model_app;
+
+import java.util.*;
+
+public class StratifyingData {
+	
+	private List<String> allOfData;
+	private List<String> labelledYes;
+	private List<String> labelledNo;
+	private int trainingRowsYes;
+	private int trainingRowsNo;
+	private List<String> trainingData;
+	private List<String> testingData;
+	private Map<String, List<String>> testAndTrainData;
+
+	
+	public Map<String, List<String>> seperateData(List<String> allOfData)
+	{
+		labelledYes = new ArrayList<>();
+		labelledNo = new ArrayList<>();
+		
+		//seperating to yes and nos 
+		for (String line : allOfData)
+		{
+			if (line.endsWith("no"))
+			{
+				labelledNo.add(line);
+			}
+			else if (line.endsWith("yes"))
+			{
+				labelledYes.add(line);
+			}
+					
+		}
+		
+		//putting 75 percent of values into training data 
+		trainingRowsYes = (int)(labelledYes.size() * 0.75);
+		trainingRowsNo = (int)(labelledNo.size() * 0.75);
+		
+		
+		//adding to training and testing data to array lists now
+		trainingData.addAll(labelledYes.subList(0, trainingRowsYes));
+		testingData.addAll(labelledYes.subList(trainingRowsYes, labelledYes.size()));
+		
+		trainingData.addAll(labelledNo.subList(0, trainingRowsNo));
+		testingData.addAll(labelledNo.subList(trainingRowsNo, labelledYes.size()));
+		
+		//inputting data in hashmap to return
+		testAndTrainData = new HashMap<>();
+		testAndTrainData.put("training data", trainingData);
+		testAndTrainData.put("testing data", testingData);
+
+
+		//returning hashmap containing the testing and training data 
+		return testAndTrainData;
+
+
+		
+	}
+
+
+}
